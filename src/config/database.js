18a7103eb45b5b8a7348817bb9d2af9ab4697bee
LucasGSeabra <51447406+LucasGSeabra@users.noreply.github.com>
2020-10-13 10:28:37 -0300

    refatorado database

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,9 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose'
+
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect("mongodb+srv://admin:admin@ldnm.i3jjc.mongodb.net/to-do?retryWrites=true&w=majority")
+const uri = "mongodb+srv://admin:admin@ldnm.i3jjc.mongodb.net/to-do?retryWrites=true&w=majority"
+
+mongoose.connect(uri, {useNewUrlParser: true})
+
+export default mongoose
